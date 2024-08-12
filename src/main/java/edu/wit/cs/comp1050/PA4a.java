654b4d39bf50ba42f287d02e89c0f6c8f9ec944c
Nathaniel Smith <Nathaniel Smith@10.0.0.15>
2024-08-12 17:31:03 -0400

    commit 8/12

--- src/main/java/edu/wit/cs/comp1050/PA4a.java
+++ src/main/java/edu/wit/cs/comp1050/PA4a.java
@@ -1,5 +1,6 @@
 package edu.wit.cs.comp1050;
 
+
 //TODO: document this class
 public class PA4a {
 	
@@ -13,6 +14,22 @@ public class PA4a {
 	 */
 	public static final String ERR_NONE = "No valid shifts found.";
 
+	public static String decrypt(String message, int shift) {
+        StringBuilder decrypted = new StringBuilder();
+
+        for (char c : message.toCharArray()) {
+            if (Character.isLetter(c)) {
+                char base = Character.isLowerCase(c) ? 'a' : 'A';
+                char decryptedChar = (char) ((c - base - shift + 26) % 26 + base);
+                decrypted.append(decryptedChar);
+            } else {
+                decrypted.append(c);
+            }
+        }
+
+        return decrypted.toString();
+    }
+	
 	/**
 	 * Outputs all shifts of the encrypted string
 	 * that contain the supplied substring
@@ -20,7 +37,7 @@ public class PA4a {
 	 * @param args command-line arguments: <encrypted string> <substring>
 	 */
 	public static void main(String[] args) {
-		// replace with your code
+		
 	}
 
 }
